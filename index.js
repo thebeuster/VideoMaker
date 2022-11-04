ad5b2e08659b10076b4588913f00116be9430e66
Rodrigo Yuji Shimoki Maeda <derekbeuster@gmail.com>
2022-11-04 17:04:57 -0300

    feat: start text robot

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
+
 const readline = require('readline-sync')
-const webdriver = require('selenium-webdriver');
-
-const By = webdriver.By
-const driver = new webdriver.Builder()
-  .forBrowser('chrome') // http://chromedriver.chromium.org/downloads
-  .build()
-
-/**
- * Função para iniciar a app para cada resultado das tendencias
- */
-async function start() {
-  asyncForEach(await selectNextTrend(), trend => {
-    questions(trend)
-  })
+const Parser = require('rss-parser');
+const unicode = require('unidecode');
+const robots = {
+  text: require('./robots/text.js'),
+  gTrends: require('./gTrends.js'),
+  wikipedia : require('./wikipedia.js')
 }
 
+async function start(){
+  const content = {} 
 
+  /*
+  * Estou modificando um pouco a sugestão de estrutura do Content, pela API da propria Wikipedia,
+  * temos varios outros dados muito valiosos, como imagens e até mesmo fonte dessas informações,
+  * o que acaba sendo muito util para futuras implementações, ser descartado isso e ser utilizado apenas a propriedade content.
+  * Obviamente, caso realmente deseje utilizar apenas a propriedade content, 
+  * podemos extrair apenas a propriedade extracts (Na função getContent, do robo da Wikipedia, excluindo as outras propriedades)
+  * retornando apenas o texto, e deixando o retorno mais rapida.
+  */
+  content.searchTerm = await askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
+  content.wikiPediaContent = await robots.wikipedia(content)
+  await robots.text(content)
+  console.log('To the next step...')
+  console.log('Exiting Program...')
 
-/**
- * Função para definir o prefixo perguntando para o usuário
- * @param {String} searchTerm - Termo da busca
- */
-function questions(searchTerm) {
-  const content = {}
+  async function askAndReturnSearchTerm () {
+      const response = readline.question('Type a Wikipedia search term or G to fetch google trends: ')
+      /*
+      * Seguindo sugestão do @HiltonWS estou modulando o Google Trends, assim tendo a possibilidade de 
+      * utilizar outros trends ou outras fontes de sugestões, apenas modificando o texto acima de questão
+      * e modificando a função/robo utilizado
+      */
+      var value = (response.toUpperCase() === 'G') ?  await robots.gTrends() : response
+      if(!value){
+        console.log('You don\'t defined any search term...')
+        console.log('Exiting Program...')
+        process.exit()
+      }
+      return value
 
-  // content.searchTerm = askAndReturnSearchTerm()
-  content.searchTerm = searchTerm
-  content.prefix = askAndReturnPrefix(searchTerm)
-
-  function askAndReturnSearchTerm() {
-    return readline.question('Type a Wikipedia search term: ')
   }
 
-  function askAndReturnPrefix(searchTerm) {
-    const prefixes = ['Who is', 'What is', 'The history of']
-    const selectedPrefixIndex = readline.keyInSelect(prefixes, `Choose an option for ${searchTerm}: `)
-    const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-    return selectedPrefixText
+  function askAndReturnPrefix(){
+      const prefix = ['Who is','What is','The history of']
+      const selectedPrefixIndex = readline.keyInSelect(prefix,'Choose an option for \''+unicode(content.searchTerm)+' \':')
+      const selectedPrefixText = prefix[selectedPrefixIndex]
+      if(!selectedPrefixText){
+        console.log('You don\'t defined a option for your term...')
+        console.log('Exiting Program...')
+        process.exit()
+      }
+      return selectedPrefixText
   }
 
-  console.log(content)
-}
+  console.log(content)	
 
-/**
- * Função para buscar uma nova tendencia no google
- */
-const selectNextTrend = async () => {
-  let searchTerm = []
-
-  // Pesquisas em Alta
-  await driver.get('https://trends.google.com/trends/trendingsearches/daily?geo=BR')
-
-  // xpath: //tagname[@attribute='value']
-  const list = await driver.findElement(By.xpath("//md-list[@class='md-list-block']"))
-  const details = await list.findElements(By.xpath("//div[@class='details']/div[@class='details-top']"))
-
-  await asyncForEach(details, async title => {
-    searchTerm.push(await title.getText())
-  })
-  await driver.close()
-
-  return searchTerm
-}
-
-/**
- * Função asyncrona para um for dos items dentro do array
- */
-async function asyncForEach(array, callback) {
-  for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
-  }
 }
 
 start()
---
+++ robots/text.js
@@ -0,0 +1,61 @@
+const sentenceBoundaryDetection = require('sbd')
+
+async function Text(content) {
+  // await fetchContentFromWikipedia(content)
+  sanitizeContent(content)
+  breakContentIntoSentences(content)
+  console.log('Build Sentences')
+
+  /*
+  *
+  * Com o Robo da Wikipedia não precisamos utilizar o Algorithmia e usar nossos credito no mesmo.
+  * 
+  * Assim sendo passo diretamente os valores para o robo de texto e utlizo o que o robo do Wikipedia retornou para mim
+  * 
+  */
+  // async function fetchContentFromWikipedia(content) {
+  //   const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+  //   const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+  //   const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
+  //   const wikipediaContent = wikipediaResponde.get()
+
+  //   content.sourceContentOriginal = wikipediaContent.content
+  // }
+
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.wikiPediaContent.content)
+    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    content.wikiPediaContent.sourceContentSanitized = withoutDatesInParentheses
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split('\n')
+      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+        if (line.trim().length === 0 || line.trim().startsWith('=')) {
+          return false
+        }
+        return true
+      })
+      return withoutBlankLinesAndMarkdown.join(' ')
+    }
+  }
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+  }
+  function breakContentIntoSentences(content) {
+    content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.wikiPediaContent.sourceContentSanitized)
+    console.log(sentences)
+      sentences.forEach((sentence) => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      })
+    })
+  }
+
+}
+
+module.exports = Text
